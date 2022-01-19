689916a06a0d69211d431f2afd953685edee87b2
norivera <89235780+norivera@users.noreply.github.com>
2022-01-18 17:01:08 -0800

    apk

---
+++ build/app/generated/source/buildConfig/debug/cpp/edu/cs4750/noe/stemcompanion/steam_companion/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package cpp.edu.cs4750.noe.stemcompanion.steam_companion;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "cpp.edu.cs4750.noe.stemcompanion.steam_companion";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
